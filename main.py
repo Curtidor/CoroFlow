6d1cba9615dfb63948425be9db368186626cf3bb
Curtidor <tannermatos18@gmail.com>
2024-11-10 23:27:26 -0400

    main

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import asyncio
 import functools
 import random
 
-from CroFlow import run_coros
+from CroFlow import run_coros, threading_run_coros
 
 
 async def fetch_data(url: str) -> str:
     # Simulate fetching data from a URL
-    sleep_time = random.randint(1, 3)
+    sleep_time = random.randint(1, 5)
+    if sleep_time == 5:
+        raise Exception("TO MUCH DOWN TIME")
+
     await asyncio.sleep(sleep_time)  # Simulate varying fetch times
     return f"Data fetched from {url}"
 
@@ -20,15 +23,20 @@ async def main():
         "https://example.com/data2",
         "https://example.com/data3",
         "https://example.com/data4",
-        "https://example.com/data5"
+        "https://example.com/data5",
+        "https://example.com/data6",
+        "https://example.com/data7",
+        "https://example.com/data8",
+        "https://example.com/data9",
+        "https://example.com/data10"
     ]
 
     # Create a list of asynchronous tasks
     tasks = [functools.partial(fetch_data, url) for url in urls]
 
     # Run the tasks concurrently with a maximum of 3 process groups
-    async for result in run_coros(tasks, timeout=4):
-        print(result)
+    async for result in run_coros(tasks, timeout=5, debug=True):
+        print(result, type(result))
 
 
 asyncio.run(main())
